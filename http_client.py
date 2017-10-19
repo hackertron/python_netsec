d05c0878d81b28e9f3a601a8098617a7df417ab3
Gur Raunaq Singh <raunaq.soni+gurraunaqsingh@gmail.com>
2017-10-19 18:16:27 +0530

    Nagar Palika

--- http_client.py
+++ http_client.py
@@ -8,3 +8,7 @@ print (data.headers)
 text = data.readlines()
 for t in text:
     print(t.decode('utf-8'))
+    
+# _array = ['bhai', 'bhai', 'bhai']
+# _array.append('Kya code likha hai bhai ne, nagar palika ko bulao mc...')
+
